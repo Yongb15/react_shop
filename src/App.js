274b507c47a24fa0257991b716749a172fe81fdd
Yongb15 <dydqls5658@naver.com>
2025-04-07 19:47:25 +0900

    스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container className="d-flex align-items-center">
-          <Navbar.Brand href="#home"className="brand-padding">집가구싶엉</Navbar.Brand>
-          <div className="flex-spacer"></div>
-          <Nav className="me-auto custom-nav">
-            <Nav.Link href="#home" >Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <div className="page-background">
+      <div className="main-box">
+        <Navbar bg="light" variant="light" className="custom-navbar">
+          <Container className="zigzag-container">
+            <Navbar.Brand href="#home">집가구싶엉</Navbar.Brand>
+            <Nav className="menu">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        {/* 여기 아래로 너 원하는 콘텐츠 추가하면 돼 */}
+        <div style={{ padding: '2rem' }}>
+          <h2>메인 콘텐츠 영역</h2>
+        </div>
+      </div>
     </div>
   );
 }
